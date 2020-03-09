452ee53cf4ce99d23f47009a79ab8055cd2a3eea
pranali1997 <pranali.lembhe77@gmail.com>
2020-03-09 16:13:10 +0530

    Added : test cases for service

--- test/service/converterApi.js
+++ test/service/converterApi.js
@@ -1,24 +1,47 @@
-var chai= require('chai');
-var chaiHttp=require('chai-http')
+var chai = require('chai');
+var chaiHttp = require('chai-http')
 var fs = require('fs');
-var app=require('../../server.js')
+var app = require('../../server.js')
 
 chai.should();
 chai.use(chaiHttp);
 
-var file=fs.readFileSync('./service/serviceJSON.json','utf-8');
-var fileInfo=JSON.parse(file);
+var file = fs.readFileSync('./service/serviceJSON.json', 'utf-8');
+var fileInfo = JSON.parse(file);
 
 describe('api checking', () => {
-    describe('test for null login', (done) => {
+    describe('test for null input', (done) => {
         chai.request(app)
-        .post('/enterValue')
-        .send(info.nullFieldInput)
-        .end((err,res)=>{
-            res.should.have.status(422);
-            done();
-        })
+            .post('/enterValue')
+            .send(fileInfo.nullFieldInput)
+            .end((err, res) => {
+                res.should.have.status(422);
+                done();
+            })
     })
-    
-    
+
+
+
+
+    describe('test for empty input', () => {
+        chai.request(app)
+            .post('./enterValue')
+            .send(fileInfo.noFieldInput)
+            .end((err, res) => {
+                res.should.have.status(500);
+                done();
+            })
+    }),
+
+        describe('test for correct input', () => {
+            chai.request(app)
+                .post('./enterValue')
+                .send(fileInfo.correctFieldInput)
+                .end((err, res) => {
+                    res.body.should.be.a("object")
+                })
+
+        })
+
+
 })
--- test/service/service.test.js
+++ test/service/service.test.js
@@ -1,82 +1,81 @@
-// const assert=require('chai').assert;
-// const service=require('../../main/source/service/service')
-// describe('quantity measurement service test cases', function(){
+const assert=require('chai').assert;
+const service=require('../../main/source/service/service')
+describe('quantity measurement service test cases', function(){
 
-//     describe('data with correct value',function(){
-//         var data1={
-//             unit:"0",
-//             firstUnit:"INCH",
-//             secondUnit:"FEET",
-//             unitValue:12
-//         }
-//         it('given correct object of inch value should return 1 feet',function(){
-//             service.unitMeasurement(data1,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                 }
-//                 console.log('data of the first test is =============================>/',data);
+    describe('data with correct value',function(){
+        var data1={
+            unit:"LENGTH",
+            firstUnit:"INCH",
+            secondUnit:"FEET",
+            unitValue:12
+        }
+        it('given correct object of inch value should return 1 feet',function(){
+            service.unitMeasurement(data1,(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
+
+                data=Math.round(data)
+                assert.equal(data,1)
+            })
+        })
+    }),
+
+    describe('data with no value',function(){
+        data2={
+            unit:"",
+            firstUnit:"",
+            secondUnit:"",
+            unitValue:0
+        }
+        it('given empty unit values should return err',function(){
+            var noValue=service.unitMeasurement(data2,(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
                 
-//                 data=Math.round(data)
-//                 assert.equal(data,1)
-//             })
-//         })
-//     }),
+            })
+            assert(data2,undefined)
+        })
 
-//     describe('data with no value',function(){
-//         data2={
-//             unit:"",
-//             firstUnit:"",
-//             secondUnit:"",
-//             unitValue:0
-//         }
-//         it('given empty unit values should return err',function(){
-//             var noValue=service.unitMeasurement(data2,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                 }
+    }),
+
+    describe('data with null value',function(){
+        data2={
+            unit:null,
+            firstUnit:null,
+            secondUnit:null,
+            unitValue:0
+        }
+        it('given null unit values should return err',function(){
+            var noValue=service.unitMeasurement(data2,(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
                 
-//             })
-//             assert(data2,undefined)
-//         })
+            })
+            assert(data2,undefined)
+        })
 
-//     }),
+    }),
 
-//     describe('data with null value',function(){
-//         data2={
-//             unit:null,
-//             firstUnit:null,
-//             secondUnit:null,
-//             unitValue:0
-//         }
-//         it('given null unit values should return err',function(){
-//             var noValue=service.unitMeasurement(data2,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                 }
+    describe('data with wrong value',function(){
+        data2={
+            unit:"MASS",
+            firstUnit:"KILOGRAM",
+            secondUnit:"GRAM",
+            unitValue:1
+        }
+        it('given wrong unit values should return err',function(){
+            var noValue=service.unitMeasurement(data2,(err,data)=>{
+                if(err){
+                    console.log(err);
+                }
                 
-//             })
-//             assert(data2,undefined)
-//         })
+            })
+            assert(data2,undefined)
+        })
 
-//     }),
+    })
 
-//     describe('data with wrong value',function(){
-//         data2={
-//             unit:"0",
-//             firstUnit:"KILOGRAM",
-//             secondUnit:"GRAM",
-//             unitValue:1
-//         }
-//         it('given wrong unit values should return err',function(){
-//             var noValue=service.unitMeasurement(data2,(err,data)=>{
-//                 if(err){
-//                     console.log(err);
-//                 }
-                
-//             })
-//             assert(data2,undefined)
-//         })
-
-//     })
-
-// })
+})
